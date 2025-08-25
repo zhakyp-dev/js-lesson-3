f5bdd69c33335b199ce4c39cd57ea0ef471105e7
Jakyp-2005 <jakypjumagulov80@gmail.com>
2025-08-25 16:55:27 +0600

    lesson-3

---
+++ index.js
@@ -0,0 +1,79 @@
+// массив, методы массивов, []
+
+// let a = 5;
+// let b = 6;
+// let c = 7;
+
+// console.log(a, b, c);
+
+// массив
+let numbers = [5, 6, 7];
+
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+
+// методы массивов
+// push(), pop(), shift(), unshift()
+numbers.push(8);
+numbers.push(9);
+numbers.push(10);
+
+numbers.unshift(4);
+numbers.unshift(3);
+numbers.unshift(2);
+numbers.unshift(1);
+
+numbers.pop();
+
+numbers.shift();
+numbers.shift();
+
+console.log(numbers);
+
+// Практика
+
+let fruits = ["Banana", "Cherry", "Apple"];
+
+fruits.push("Juice");
+
+// push("Juice"), pop(), shift(), unshift()
+console.log(fruits);
+
+let users = [
+  {
+    id: 1,
+    name: "Artur",
+    age: 14,
+  },
+  {
+    id: 2, 
+    name: "Nurel",
+    age: 12,
+  }
+];
+
+console.log(users)
+// console.log()
+
+// Тапшырма 1
+// Массив түз: ["orange", "mango", "grape"]
+// Консолго массивди жана анын узундугун чыгар.
+// Максат: .length колдонуп үйрөнүү
+
+// Тапшырма 2
+// Массив түз: ["pen", "pencil", "book", "eraser"]
+// book деген элементти console.log() менен чыгар.
+// Максат: индекс менен кайрылуу
+
+// Тапшырма 3
+// Массив: ["red", "green"]
+// Аягына "blue" кош.
+// .push() колдон
+
+// Тапшырма 4
+// Массив: [100, 200]
+// Башына 50 кош.
+// .unshift() колдон
